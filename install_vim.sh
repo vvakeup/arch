a1df8c5d084b22012a0560cb042c0cb962b479db
vvakeup <144046747+vvakeup@users.noreply.github.com>
2023-09-14 15:46:08 +0300

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -24,14 +24,35 @@ set colorcolumn=+1
 set encoding=utf-8                                                          
 set nobackup                                                                
 set nowritebackup                                                           
-set noswapfile                                                              
+set noswapfile      
+set noundofile
 set updatetime=300                                                          
 set signcolumn=yes  
+set set nocompatible
 
-call plug#begin()
-Plug 'junegunn/vim-easy-align'
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
+call plug#begin('$VIM/vimfiles/plugged')
+" Plug 'junegunn/vim-easy-align'
+" Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'sheerun/vim-polyglot'
+Plug 'sainnhe/everforest'
 call plug#end()
+
+ " Important!!
+if has('termguicolors')
+  set termguicolors
+endif
+" For dark version.
+set background=dark
+" Set contrast.
+" This configuration option should be placed before `colorscheme everforest`.
+" Available values: 'hard', 'medium'(default), 'soft'
+let g:everforest_background = 'medium'
+let g:everforest_better_performance = 1
+let g:everforest_enable_italic = 1
+let g:everforest_diagnostic_line_highlight = 1
+let g:everforest_cursor = 'orange'
+colorscheme everforest
+
 EOF
 
 echo "Vim install finished!"
